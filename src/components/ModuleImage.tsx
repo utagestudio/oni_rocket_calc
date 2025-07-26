72a43642656f47e8367dd87230536066a908da08
utagestudio <utagestudio+github@gmail.com>
2025-07-26 17:54:42 +0900

    Add: Buttons to manage module instances
    `ModuleImage`にプラス・マイナスボタンを追加しました。これにより、モジュールのインスタンスを動的に追加・削除する操作が可能になりました。また、それに伴い関連するSassスタイルを更新し、ボタン部分のレイアウトを調整しました。これらの変更により、UIの操作性と視認性が向上しました。

--- src/components/ModuleImage.tsx
+++ src/components/ModuleImage.tsx
@@ -1,13 +1,32 @@
 import "./ModuleImage.sass"
+import useModules from '@/hooks/useModules'
 
 type Props = {
   module: tItem
 }
 
 function ModuleImage({module}: Props) {
+  const {addModule, removeModule} = useModules()
+
+  const build = () => {
+    addModule(module)
+  }
+
+  const destroy = () => {
+    removeModule(module)
+  }
+
   return <>
     <div className="ModuleImage">
-      <img className="ModuleImage_img" src={`/assets/images/${module.image}`} srcSet={`/assets/images/${module.image2x} 2x, /assets/images/${module.image} 1x`} alt={module.name} />
+      <div className="ModuleImage_imageWrap">
+        <img className="ModuleImage_image" src={`/assets/images/${module.image}`} srcSet={`/assets/images/${module.image2x} 2x, /assets/images/${module.image} 1x`} alt={module.name} />
+      </div>
+      <div className="ModuleImage_buttons">
+        {module.multiple && <>
+          <button className="ModuleImage_button -plus" onClick={build}>+</button>
+          <button className="ModuleImage_button -minus" onClick={destroy}>-</button>
+        </>}
+      </div>
     </div>
   </>
 }
