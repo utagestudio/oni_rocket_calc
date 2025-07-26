fcaa77cd9dae39077ab232d08ef5ff2a94e756f7
utagestudio <utagestudio+github@gmail.com>
2025-07-26 18:17:51 +0900

    Add: Center main content and adjust wrapper styles
    `main`にセンタリングのためのスタイルを追加し、全体のレイアウトを調整しました。また、新たに`wrapper`クラスを導入し、最大幅や余白のスタイルを適用しました。これにより、UIのバランスと視認性が向上しました。併せて背景画像の設定を`main`に移動し、コードの整理を行いました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,16 @@ export default function Home() {
   return <>
     <div className={styles.page}>
       <div className={styles.main}>
-        <div className={styles.title}>Rocket Calculator</div>
-        <div className={styles.reset}><ResetButton /></div>
-        <div className={styles.copyright}><Copyright /></div>
-        <div className={styles.content}>
-          <ModuleSelector />
+        <div className={styles.wrapper}>
+          <div className={styles.title}>Rocket Calculator</div>
+          <div className={styles.reset}><ResetButton /></div>
+          <div className={styles.copyright}><Copyright /></div>
+          <div className={styles.content}>
+            <ModuleSelector />
+          </div>
+          <div className={styles.distance}><Distance /></div>
+          <div className={styles.rocket}><Rocket /></div>
         </div>
-        <div className={styles.distance}><Distance /></div>
-        <div className={styles.rocket}><Rocket /></div>
       </div>
       <div className={styles.footer}><Footer /></div>
     </div>
