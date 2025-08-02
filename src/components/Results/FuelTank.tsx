936c06003afbb9ff5c0eee0a665bbe390fbed0c0
utagestudio <utagestudio+github@gmail.com>
2025-08-02 18:42:39 +0900

    Add: FuelTank component and integrate with Results
    `FuelTank`コンポーネントを新規作成し、`Results`に統合しました。これにより、必要な燃料タンクの数量や各タンクの容量を動的に算出・表示可能になりました。併せて、関連するSassファイルを追加し、レイアウトを調整しました。

---
+++ src/components/Results/FuelTank.tsx
@@ -0,0 +1,25 @@
+import './FuelTank.sass'
+type Props = {
+  required: number
+}
+
+function FuelTank({required}: Props) {
+  const requiredTanks = Math.ceil(required / 900)
+
+  return <>
+    <div className="FuelTank">
+      <ul className="FuelTank_list">
+        {Array.from({length: requiredTanks}, (_, i) => {
+          const capacity = requiredTanks === i + 1 ? (required % 900) : 900
+
+          return <li className="FuelTank_item" key={i}>
+            <div className="FuelTank_image"><img className="FuelTank_img" src="/assets/images/img_fuel_tank.webp" srcSet="/assets/images/img_fuel_tank.webp 1x, /assets/images/img_fuel_tank@2x.webp 2x" alt="" /></div>
+            <div className="FuelTank_capacity">{capacity}kg</div>
+          </li>
+        })}
+      </ul>
+    </div>
+  </>
+}
+
+export default FuelTank
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,23 +1,35 @@
 import './Results.sass'
 import SelectedModules from '@/components/Results/SelectedModules'
+import FuelTank from '@/components/Results/FuelTank'
+import OxidizerTank from '@/components/Results/OxidizerTank'
 type Props = {}
 
 function Results({}: Props) {
+
+  const fuel_required = 1234
+  const oxidizer_required = 1234
+
   return <>
     <div className="Results">
       <div className="Results_wrap">
-        <div className="Results_selected">
+        <div className="Results_cell -selected">
           <div className="Results_title">Selected Modules</div>
-          <SelectedModules />
+          <div className="Results_content"><SelectedModules /></div>
         </div>
-        <div className="Results_fuel">
-          <div className="Results_title">FUEL TANKS<div className="Results_total">1,000kg</div></div>
+        <div className="Results_cell -fuel">
+          <div className="Results_title">FUEL TANKS<div className="Results_total">REQUIRED: {fuel_required.toLocaleString()}kg</div></div>
+          <div className="Results_content">
+            <FuelTank required={fuel_required} />
+          </div>
         </div>
-        <div className="Results_oxidizer">
-          <div className="Results_title">OXIDIZER TANKS<div className="Results_total">1,000kg</div></div>
+        <div className="Results_cell -oxidizer">
+          <div className="Results_title">OXIDIZER TANKS<div className="Results_total">REQUIRED: {oxidizer_required.toLocaleString()}kg</div></div>
+          <div className="Results_content">
+          </div>
         </div>
-        <div className="Results_thruster">
+        <div className="Results_cell -thruster">
           <div className="Results_title">For THRUSTER</div>
+          <div className="Results_content"></div>
         </div>
       </div>
     </div>
