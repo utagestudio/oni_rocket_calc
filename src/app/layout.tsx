309ef7319c606013f904744ab111ec1b6b72eca5
utagestudio <utagestudio+github@gmail.com>
2025-08-16 19:04:58 +0900

    Add: SEO metadata and OGP settings for Rocket Calculator
    `metadata`プロパティにSEOとOGPに関連する設定を追加しました。
    Googleなどの検索エンジン向けに`canonical`、`openGraph`、そして`twitter`データを設定し、SNSでのシェア時に見栄えや情報伝達が改善されます。また、画像ファイル`ogp.png`をプロジェクトに追加しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,34 @@ const barriecito = Barriecito({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://rocket-calc.utage.games'),
   title: "Rocket Calculator for Oxygen Not Included Base Game | UTAGE.GAMES",
   description: "Automatic fuel amount calculator for rockets in Oxygen Not Included",
+  alternates: {
+    canonical: '/', // ルートの canonical
+  },
+  openGraph: {
+    type: 'website',
+    siteName: 'Rocket Calculator for Oxygen Not Included Base Game | UTAGE.GAMES',
+    url: 'https://rocket-calc.utage.games',
+    title: 'Rocket Calculator for Oxygen Not Included Base Game | UTAGE.GAMES',
+    description: 'Automatic fuel amount calculator for rockets in Oxygen Not Included',
+    images: [
+      {
+        url: '/assets/ogp.png',
+        width: 1200,
+        height: 630,
+        alt: 'design',
+      },
+    ],
+    locale: 'en',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@utage_studio', // 公式アカウント
+    creator: '@utage_studio', // 作成者アカウント
+  },
+
 };
 
 export default function RootLayout({
