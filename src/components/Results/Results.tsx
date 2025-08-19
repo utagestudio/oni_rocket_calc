aef197d62355d2bffd5cab33f341a22c703545f7
utagestudio <utagestudio+github@gmail.com>
2025-08-19 18:06:08 +0900

    Add: "Unreached" state to Results component #17
    `Results`コンポーネントに対して、新しい`-unreached`状態を追加しました。計算結果が負の値となった場合に表示され、視覚的なフィードバックを提供します。また、関連するスタイルとレイアウトを調整し、UIの一貫性を維持しています。これにより、ユーザーは無効な計算結果を直感的に認識できるようになりました。

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -46,44 +46,54 @@ function Results({}: Props) {
           <div className="Results_content"><SelectedModules isShown={isShowSelectedModuleArea} onToggle={toggleSelectedModuleArea} /></div>
         </div>
 
-        {engine.name === "Steam Engine" &&
-          <div className="Results_cell -steam">
-            <div className="Results_title">Steam</div>
-            <div className="Results_content">
-              <SteamTank required={amount} />
+        {amount < 0 &&
+          <div className="Results_cell -unreached">
+            <div className="Results_content -unreached">
+              {isCalculating ? '---' : 'Unreached'}
             </div>
           </div>
         }
-
-        {engine.name !== "Steam Engine" && <>
-          <div className="Results_cell -fuel">
-            <div className="Results_title">Fuel Tanks</div>
-            <div className="Results_content">
-              <FuelTank required={amount} />
+        {amount >= 0 && <>
+          {engine.name === "Steam Engine" &&
+            <div className="Results_cell -steam">
+              <div className="Results_title">Steam</div>
+              <div className="Results_content">
+                <SteamTank required={amount} />
+              </div>
             </div>
-          </div>
-          <div className="Results_cell -oxidizer">
-            <div className="Results_title">Oxidizer Tanks</div>
-            <div className="Results_content">
-              <OxidizerTank required={amount} />
+          }
+
+          {engine.name !== "Steam Engine" && <>
+            <div className="Results_cell -fuel">
+              <div className="Results_title">Fuel Tanks</div>
+              <div className="Results_content">
+                <FuelTank required={amount} />
+              </div>
+            </div>
+            <div className="Results_cell -oxidizer">
+              <div className="Results_title">Oxidizer Tanks</div>
+              <div className="Results_content">
+                <OxidizerTank required={amount} />
+              </div>
+            </div>
+          </>}
+
+          {thruster.length > 0 && <>
+            <div className="Results_cell -thruster">
+              <div className="Results_title">For Thruster</div>
+              <div className="Results_content">
+                <ForThruster />
+              </div>
+            </div>
+          </>}
+          <div className="Results_cell -amount">
+            <div className="Results_title">Fuel Amount</div>
+            <div className="Results_content -amount">
+              <div className="Results_total">{isCalculating ? '---' :amount.toLocaleString()}</div>kg
             </div>
           </div>
         </>}
 
-        {thruster.length > 0 && <>
-          <div className="Results_cell -thruster">
-            <div className="Results_title">For Thruster</div>
-            <div className="Results_content">
-              <ForThruster />
-            </div>
-          </div>
-        </>}
-        <div className="Results_cell -amount">
-          <div className="Results_title">Fuel Amount</div>
-          <div className="Results_content -amount">
-            <div className="Results_total">{isCalculating ? '---' :amount.toLocaleString()}</div>kg
-          </div>
-        </div>
       </div>
     </div>
   </>
