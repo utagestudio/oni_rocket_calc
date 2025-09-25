47ad394293bde9b1e889b19ea91d5140c6bc80e5
utagestudio <utagestudio+github@gmail.com>
2025-09-26 01:11:25 +0900

    Refactor: Move and refactor SelectedModules component #19
    `SelectedModules`コンポーネントを`/Rocket`ディレクトリへ移動し、ファイル構造を整理しました。また、`Results`コンポーネントから該当部分を削除し、`Rocket`コンポーネントへ統合しました。この変更により、関連する機能が一箇所に統合され、可読性およびコードの管理性が向上しました。スタイルの最適化も実施し、冗長な記述を削減しました。

--- src/components/Rocket/Rocket.tsx
+++ src/components/Rocket/Rocket.tsx
@@ -3,15 +3,23 @@
 import './Rocket.sass'
 import useModules from '@/hooks/useModules'
 import ModuleImage from '@/components/ModuleImage'
+import SelectedModules from '@/components/Rocket/SelectedModules'
+import {useCallback, useState} from 'react'
 type Props = {}
 
 function Rocket({}: Props) {
   const modules = useModules()
+  const [isShowSelectedModuleArea, setIsShowSelectedModuleArea] = useState(false)
+
+  const toggleSelectedModuleArea = useCallback(() => {
+    setIsShowSelectedModuleArea(prev => !prev)
+  }, [])
 
   return <>
     <div className="Rocket">
       <div className="Rocket_wrap">
-        <ul className="Rocket_modules">
+        <div className={`Rocket_names ${isShowSelectedModuleArea ? '-show' : ''}`} ><SelectedModules isShown={isShowSelectedModuleArea} onToggle={toggleSelectedModuleArea} /></div>
+        <ul className={`Rocket_modules ${isShowSelectedModuleArea ? '' : '-show'}`}>
           <li className="Rocket_module"><ModuleImage module={modules.head} /></li>
           { modules.modules.length > 0 && <>
             { modules.modules.map(((m, i) => {return <li className="Rocket_module" key={i}><ModuleImage module={m} /></li>})) }
@@ -27,6 +35,7 @@ function Rocket({}: Props) {
           </>}
           <li className="Rocket_module"><ModuleImage module={modules.engine} /></li>
         </ul>
+        <button className="Rocket_change" onClick={toggleSelectedModuleArea}>CHANGE</button>
       </div>
     </div>
   </>
