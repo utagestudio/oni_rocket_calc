d71e5cd57ec4fb52ca82291797fa395d0269ce95
utagestudio <utagestudio+github@gmail.com>
2025-09-26 00:00:30 +0900

    Add: Icon and styles for ResetButton component #25
    `ResetButton`コンポーネントにリセットアイコンを追加し、スタイルを更新しました。これにより、ボタンのデザインが向上し、ユーザーが直感的にリセット機能を認識しやすくなりました。また、ホバー時のアニメーションやレスポンシブレイアウトを考慮したスタイルを`ResetButton.sass`に定義しました。`page.module.sass`も調整し、全体のデザインの一貫性を保っています。

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -9,7 +9,8 @@ function ResetButton({}: Props) {
   const {reset} = useModules()
   return <>
     <button onClick={reset} className="ResetButton">
-      Reset
+      <span className="ResetButton_icon"><img className="ResetButton_img" src={`/assets/images/ico_reset.svg`} alt="reset" width={24} /></span>
+      <span className="ResetButton_label">Reset</span>
     </button>
   </>
 }
